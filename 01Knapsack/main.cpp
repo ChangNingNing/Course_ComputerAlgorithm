0724040a32887a3f759100f2bbf00813bb575a30
ChangNingNing <r05922087@ntu.edu.tw>
2016-12-15 17:54:06 +0800

    modify

--- 01Knapsack/main.cpp
+++ 01Knapsack/main.cpp
@@ -5,7 +5,6 @@
 
 typedef struct {
 	int v, w;
-	float uV;
 } Object;
 
 int W, N;
@@ -14,13 +13,11 @@ Object obj[MAXN];
 
 int compare(const void *a, const void *b){
 	Object *A = (Object *)a, *B = (Object *)b;
-	if(A->uV > B->uV) return -1;
-	else if(A->uV < B->uV) return 1;
-	if(A->w > B->w) return -1;
+	if((float)A->v/A->w > (float)B->v/B->w) return -1;
 	return 1;
 }
 
-float UGreedy(int index, int w){
+int UGreedy(int index, int w){
 	float ub = 0;
 	for(int i=index; i<N && w < W; i++){
 		float tmp = W - w;
@@ -29,7 +26,7 @@ float UGreedy(int index, int w){
 		w += k * obj[i].w;
 		ub += k * obj[i].v;
 	}
-	return ub;
+	return (int)ub;
 }
 
 int LGreedy(int index, int w){
@@ -44,7 +41,8 @@ int LGreedy(int index, int w){
 	return lb;
 }
 
-void DFS(int index, int w, int v, float ub){
+void DFS(int index, int w, int v){
+	int ub = v + UGreedy(index, w);
 	if(w > W) return;
 	if(ub <= bound) return;
 	if(index >= N){
@@ -52,27 +50,8 @@ void DFS(int index, int w, int v, float ub){
 		return;
 	}
 
-	int tmp = W - w;
-	float ub_tmp[2];
-	ub_tmp[0] = ub_tmp[1] = -1;
-	if(tmp >= obj[index].w)
-		ub_tmp[0] = UGreedy(index + 1, w + obj[index].w);
-	ub_tmp[1] = UGreedy(index + 1, w);
-	//int lb_tmp = v + LGreedy(index + 1, w);
-	//bound = (lb_tmp > bound)? lb_tmp: bound;
-
-	if(ub_tmp[0] < 0){
-		DFS(index + 1, w, v, v + ub_tmp[1]);
-		return;
-	}
-	if(ub_tmp[0] > ub_tmp[1]){	
-		DFS(index + 1, w + obj[index].w, v + obj[index].v, v+obj[index].v+ub_tmp[0]);
-		DFS(index + 1, w, v, v + ub_tmp[1]);
-	}
-	else{
-		DFS(index + 1, w, v, v + ub_tmp[1]);
-		DFS(index + 1, w + obj[index].w, v + obj[index].v, v+obj[index].v+ub_tmp[0]);
-	}
+	DFS(index + 1, w, v);
+	DFS(index + 1, w + obj[index].w, v + obj[index].v);
 	return;
 }
 
@@ -83,12 +62,10 @@ int main(){
 		scanf("%d %d", &v, &w);
 		obj[i].v = v;
 		obj[i].w = w;
-		obj[i].uV = (float)v/w;
 	}
 	qsort(obj, N, sizeof(obj[0]), compare);
-	float ub = UGreedy(0, 0);
 	bound = LGreedy(0, 0);
-	DFS(0, 0, 0, ub);
+	DFS(0, 0, 0);
 	printf("%d\n", bound);
 	return 0;
 }
